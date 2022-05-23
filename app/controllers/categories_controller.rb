a335563f6acf4c0284029b698488cfc4128de0a7
Piero Lescano <pierolescano@protonmail.com>
2022-05-23 18:46:14 -0500

    feat(views): Display basic information

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,11 @@
 class CategoriesController < ApplicationController
+  load_and_authorize_resource
+
   def index; end
+
+  def show
+    @transactions = @category.expenses
+  end
 
   def new; end
 
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,4 @@
 class TransactionsController < ApplicationController
-  def index; end
-
   def new; end
 
   def create; end
