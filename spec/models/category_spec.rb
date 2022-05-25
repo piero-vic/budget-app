f1b099dd9bafa196cd609d94273b42e2c7617cc1
Piero Lescano <pierolescano@protonmail.com>
2022-05-24 19:04:42 -0500

    test(models): Add tests for Category methods

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -25,4 +25,44 @@ RSpec.describe Category, type: :model do
     expect(subject.user.id).to eql(1)
     expect(subject.user.name).to eql('John Doe')
   end
+
+  describe '#total_value' do
+    context 'when there are no expenses' do
+      it 'should return 0' do
+        expect(subject.total_value).to eql(0)
+      end
+    end
+
+    context 'when there are expenses' do
+      before do
+        Expense.create(name: 'Eggs', amount: 9.99, user: subject.user, category: subject)
+        Expense.create(name: 'Rice', amount: 19.99, user: subject.user, category: subject)
+        Expense.create(name: 'Veggies', amount: 5.99, user: subject.user, category: subject)
+      end
+
+      it 'should return the sum of the expenses amounts' do
+        expect(subject.total_value).to eql(35.97)
+      end
+    end
+  end
+
+  describe '#last_modification' do
+    context 'when there are no expenses' do
+      it 'should return nil' do
+        expect(subject.last_modification).to be_nil
+      end
+    end
+
+    context 'when there are expenses' do
+      before do
+        Expense.create(name: 'Eggs', amount: 9.99, user: subject.user, category: subject)
+        Expense.create(name: 'Rice', amount: 19.99, user: subject.user, category: subject)
+        @last_expense = Expense.create(name: 'Veggies', amount: 5.99, user: subject.user, category: subject)
+      end
+
+      it 'should return the last modification' do
+        expect(subject.last_modification).to eql(@last_expense.created_at)
+      end
+    end
+  end
 end
