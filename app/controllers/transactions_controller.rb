456436770900dbce1374471a32664852469b60fb
Piero Lescano <pierolescano@protonmail.com>
2022-05-26 16:28:52 -0500

    feat(controller): Implement action to delete a transaction

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,7 +1,14 @@
 class TransactionsController < ApplicationController
+  load_and_authorize_resource :category
+
   def new; end
 
   def create; end
 
-  def destroy; end
+  def destroy
+    @transaction = @category.expenses.find_by_id(params[:id])
+    authorize! :destroy, @transaction
+    @transaction.destroy
+    redirect_to category_path(@category.id), notice: 'Transaction deleted'
+  end
 end
