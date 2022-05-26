faf31ce971e6ba2e87a8642122d27f13e173812f
Piero Lescano <pierolescano@protonmail.com>
2022-05-26 12:55:40 -0500

    feat(views): Add form to create a new category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,9 +7,24 @@ class CategoriesController < ApplicationController
     @transactions = @category.expenses
   end
 
-  def new; end
+  def new
+    @icons = %w[basket-shopping shirt credit-card bus car bicycle gamepad dumbbell paw graduation-cap hospital
+                kit-medical]
+  end
 
-  def create; end
+  def create
+    if @category.save
+      redirect_to categories_path, notice: 'New category added'
+    else
+      redirect_to new_category_path, alert: 'Something went wrong, try again!!'
+    end
+  end
 
   def destroy; end
+
+  protected
+
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 end
