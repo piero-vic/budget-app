24611972f86c3e90ccc6670eecefb45181a0f382
Piero Lescano <pierolescano@protonmail.com>
2022-05-27 19:47:26 -0500

    build: Change icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 john = User.create(name: 'John Doe', email: 'john.doe@example.com', password: 'password123')
 
-john_groceries = Category.create(name: 'Groceries', icon: 'basket-shopping', user: john)
-john_entertainment = Category.create(name: 'Entertainment', icon: 'gamepad', user: john)
+john_groceries = Category.create(name: 'Groceries', icon: 'basket', user: john)
+john_entertainment = Category.create(name: 'Entertainment', icon: 'controller', user: john)
 john_health = Category.create(name: 'Health', icon: 'hospital', user: john)
 
 Expense.create(name: 'Eggs', amount: 9.99, user: john, category: john_groceries)
@@ -10,7 +10,7 @@ Expense.create(name: 'Pills', amount: 5.99, user: john, category: john_health)
 
 caroline = User.create(name: 'Caroline Stone', email: 'caroline.stone@example.com', password: 'password123')
 
-caroline_groceries = Category.create(name: 'Groceries', icon: 'shopping-cart', user: caroline)
+caroline_groceries = Category.create(name: 'Groceries', icon: 'basket', user: caroline)
 caroline_health = Category.create(name: 'Health', icon: 'hospital', user: caroline)
 
 Expense.create(name: 'Eggs', amount: 9.99, user: caroline, category: caroline_groceries)
